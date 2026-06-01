076eb91040a0e67f5f9aabb895c4e6ac170dfb7a
Stephanie Johnson <johnson.stephanie076@gmail.com>
2026-05-31 17:41:36 -0700

    Test fixtures conform to no header

--- src/forkwise/fork_db.py
+++ src/forkwise/fork_db.py
@@ -8,6 +8,8 @@ Copyright (c) 2026 Stephanie Johnson
 
 import logging
 
+from dataclasses import fields
+
 from dbcommons.db_conn import DBConn
 from forkwise.dataclasses import Ingredient
 
@@ -18,23 +20,30 @@ class ForkDB(DBConn):
         super().__init__(user=user, pw=pw, db_name=db_name)
         self._logger = logging.getLogger(__name__)
     
-    def add_ingredients_from_csv(self, path_to_ingr_csv: str) -> None:
-        # Don't need a staging table for this action
+    def add_ingredients_from_csv(self, path_to_ingr_csv: str) -> int:
         # Will skip any row for which (name, unitary_amount, units) are already in the db.
+        # Returns number of rows added to Ingredients table.
 
         # TODO add some input handling for this csv in BLL
         ingr_col_defs = [(f.name, f.metadata['sql_type']) for f in fields(Ingredient)]
 
-        # TODO how does import_file handle dups that aren't allowed in the db? Does this throw an error?
-        # Should probably wrap in a try-except
-        r = self._import_file(csv_columns=ingr_col_defs, dest_table="ingredients", csv_path=path_to_ingr_csv)
+        rows_staged = self.csv_to_staging(csv_path=path_to_ingr_csv, csv_columns=ingr_col_defs)
 
-        if r==0:
-            self._logger.info("Failed to import ingredients csv")
+        if rows_staged == 0:
+            self._logger.info("No ingredients loaded from source file to staging table; no ingredeints will be added to db")
+            return 0
         
-        # TODO without loading first into a staging table, can't get num rows added ... 
-        # rows_added = self.execute_query(balances_query, (accnt_name,))
-        # return len(rows_added)
+        col_names = ", ".join(f'{a}' for a, _ in ingr_col_defs)
+        col_names_staging = ", ".join(f's.{a}' for a, _ in ingr_col_defs)
+        
+        # CHECK not sure this works
+        # CHECk that this skips anything already in db (violation of unique constraint)
+        ingr_query = "INSERT INTO ingredients (%s) " \
+            "SELECT %s" \
+            "FROM staging AS s " \
+            "RETURNING *;"
+        rows_added = self.execute_query(ingr_query, (col_names,col_names_staging))
+        return len(rows_added)
 
     def add_recipe_from_staging(self) -> None:
         # This does go through a staging table first.
