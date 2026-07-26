d7b12c2876dd1f23f6617a202068e7550e40d1eb
Stephanie Johnson <johnson.stephanie076@gmail.com>
2026-07-26 07:57:01 -0700

    create new CLI

---
+++ src/forkwise/display_meal_totals.py
@@ -0,0 +1,44 @@
+# Simple CLI to display information about a meals eaten in a date range.
+# 
+# Copyright (c) 2026 Stephanie Johnson
+
+import sys
+import logging
+import yaml
+
+from datetime import date
+from typing import List
+
+from forkwise.utils import DEFAULT_LOGGING_FORMAT, CONFIG_PATH, calc_totals_per_serving
+from forkwise.fork_db import ForkDB
+
+def display_meals_info(date_range: List[date], username: str, pw: str, path_to_config: str=CONFIG_PATH):
+    """
+    Retrieve info on the recipe from the db, calculate info per serving, print info to command line.
+    """
+
+    with open(path_to_config, "r") as config_file:
+        config = yaml.safe_load(config_file)
+        db_name = config["db"]["db_name"]
+
+    conn = ForkDB(user=username, pw=pw, db_name=db_name)
+    recipe = conn.get_recipe_totals(recipe_name=recipe_name)
+    conn.close()
+
+    totals = calc_totals_per_serving(recipe=recipe)
+
+    print(f"Nutritional values for recipe {recipe_name}, serving size {recipe.servings_amt} {recipe.servings_units}:")
+    print(f"Calories per serving: {round(totals.cal,1)}")
+    print(f"Grams of fat per serving: {round(totals.fat_grams,1)}")
+    print(f"Grams of protein per serving: {round(totals.protein_grams,1)}")
+    print(f"Grams of total carbs per serving: {round(totals.carb_grams,1)}, including {round(totals.fiber_grams,1)} of fiber and {round(totals.sugar_grams,1)} of sugar")
+
+if __name__ == "__main__":
+    logger = logging.getLogger(__name__)
+
+    if len(sys.argv) != 4:
+        raise ValueError("fork_init takes 3 args: (1) user name to connect to the db, (2) user pw, (3) recipe name to display")
+    
+    logging.basicConfig(level="INFO", format=DEFAULT_LOGGING_FORMAT)
+    
+    display_recipe_info(recipe_name=sys.argv[3], username=sys.argv[1], pw=sys.argv[2])
